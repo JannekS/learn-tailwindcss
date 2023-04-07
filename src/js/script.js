c6e98ec0a3aa7e2f3fafcd6f119628b5b271bdb0
Jannek <janneks@tutanota.com>
2023-04-07 19:26:06 +0200

    Made styling of header responsive to scrolling.

--- src/js/script.js
+++ src/js/script.js
@@ -1,9 +1,22 @@
 const playBtn = document.getElementById("play-btn");
 const backBtn = document.getElementById("back-btn");
 const cardContainer = document.getElementById("card-container");
+const header = document.getElementById("header");
 
 playBtn.addEventListener("click", flipCard);
 backBtn.addEventListener("click", flipCard);
+
+window.onscroll = function () {
+  if (window.scrollY > 40) {
+    if (!header.classList.contains("header-transparent")) {
+      header.classList.remove("header-full");
+      header.classList.add("header-transparent");
+    }
+  } else {
+    header.classList.remove("header-transparent");
+    header.classList.add("header-full");
+  }
+};
 
 function flipCard() {
   cardContainer.classList.toggle("[transform:rotateY(180deg)]");
