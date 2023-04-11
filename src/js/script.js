cd7da375e24d3a0042e8aeda953104fd3905148e
Jannek <janneks@tutanota.com>
2023-04-11 21:07:22 +0200

    Added possibility to switch between stats and code in code card.

--- src/js/script.js
+++ src/js/script.js
@@ -1,10 +1,19 @@
 const playBtn = document.getElementById("play-btn");
 const backBtn = document.getElementById("back-btn");
+const statsCodeBtn = document.getElementById("stats-code-btn");
+const codeBtnLabel = document.getElementById("code-btn-label");
+const codeBtnImg = document.getElementById("code-btn-img");
+
 const cardContainer = document.getElementById("card-container");
 const header = document.getElementById("header");
 
+const codeInfoBox = document.getElementById("code-info");
+const codeExampleBox = document.getElementById("code-example");
+
 playBtn.addEventListener("click", flipCard);
 backBtn.addEventListener("click", flipCard);
+
+statsCodeBtn.addEventListener("click", toggleInfoCode);
 
 window.onscroll = function () {
   if (window.scrollY > 40) {
@@ -20,4 +29,16 @@ window.onscroll = function () {
 
 function flipCard() {
   cardContainer.classList.toggle("[transform:rotateY(180deg)]");
+}
+
+function toggleInfoCode() {
+  if (!codeExampleBox.classList.contains("hidden")) {
+    codeBtnImg.src = "/icons/code-solid.svg";
+    codeBtnLabel.innerHTML = "Show Code";
+  } else {
+    codeBtnImg.src = "/icons/file-code-regular.svg";
+    codeBtnLabel.innerHTML = "Show Stats";
+  }
+  codeInfoBox.classList.toggle("hidden");
+  codeExampleBox.classList.toggle("hidden");
 }
